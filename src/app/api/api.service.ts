3f6992b0be618da19b6898a665a883b2b1760c53
Diego Rojo <diego.rojo@tsoftglobal.com>
2019-07-03 13:00:05 -0300

    Ajustes

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
 import Employee from './domain/Employee';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root',
 })
 export default class ApiService {
 
-    constructor(private http: HttpClient) { }
+    private readonly baseURL: string;
 
+    constructor(private http: HttpClient) {
+        this.baseURL = environment.APIEndpoint;
+    }
 
     public findAllEmployees(): Promise<Array<Employee>> {
-        return this.http.get<Array<Employee>>('https://localhost:5001/api/employees')
-            .toPromise().then((data) => {
-                return data;
-            });
+        return this.http.get<Array<Employee>>(this.baseURL + 'api/employees')
+            .toPromise();
         //return Promise.resolve([new Employee(1, 28695048, "Diego Rojo")]);
     }
 }
