6946479aacbc5e422663f0c48bc5ba4b83ac4476
diegorojotsoftlatamcom <47641640+diegorojotsoftlatamcom@users.noreply.github.com>
2019-12-19 18:07:45 -0300

    Update build-hook-command.sh

--- scripts/build-hook-command.sh
+++ scripts/build-hook-command.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo 'EJECUTANDO build-hook-command en'$PWD
-echo 'VALOR VARIABLE' $MY_VAR
-echo 'VARIABLES DE ENTORNO:' bash -c "set"
+echo 'VALOR VARIABLE MY_VAR ' $MY_VAR
+echo 'VALOR VARIABLE VARIABLE ' $VARIABLE
+# echo 'VARIABLES DE ENTORNO:' bash -c "set"
 exit 0
